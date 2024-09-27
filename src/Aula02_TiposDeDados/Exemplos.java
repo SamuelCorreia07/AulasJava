3935ea42984d6d553c8b525d4413f2a8cf6cac1a
Samuel Correia <thenenouno1@gmail.com>
2024-09-27 09:30:25 -0300

    aula 27/09/2024 tipos de dados

--- src/Aula02_TiposDeDados/Exemplos.java
+++ src/Aula02_TiposDeDados/Exemplos.java
@@ -4,7 +4,7 @@ public class Exemplos {
     //public static void main == psvm
     public static void main(String[] args) {
         System.out.println("Hello and welcome! \nOla Mundo!!!");
-
+        System.out.println();
         int anoNasc;
         double peso;
         char sexo;
@@ -13,7 +13,7 @@ public class Exemplos {
         anoNasc = 1980;
         peso = 65.7;
         sexo = 'M';
-
+        canhoto = false;
         double altura = 1.8;
 
         int contador = 20;
@@ -22,5 +22,14 @@ public class Exemplos {
         var x = 2;
         var y = 10.3;
         var z = true;
+
+        int n1 = 10;
+        int n2 = ++n1;
+        int n3 = 10;
+        int n4 = n3++;
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println(n4);
     }
 }
---
+++ src/Aula02_TiposDeDados/Exercicios/Exercicio03.java
@@ -0,0 +1,10 @@
+package Aula02_TiposDeDados.Exercicios;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        int variavel = 10;
+        double varDaVar = variavel;
+
+        System.out.print(varDaVar);
+    }
+}
