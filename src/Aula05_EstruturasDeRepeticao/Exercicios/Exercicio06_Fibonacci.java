765a27cf25bf00198c7ed100dfe74c98141d6b73
Samuel Correia <thenenouno1@gmail.com>
2024-11-01 11:01:09 -0300

    Aula 01/11/2024

--- src/Aula05_EstruturasDeRepeticao/Exercicios/Exercicio06_Fibonacci.java
+++ src/Aula05_EstruturasDeRepeticao/Exercicios/Exercicio06_Fibonacci.java
@@ -2,8 +2,18 @@ package Aula05_EstruturasDeRepeticao.Exercicios;
 
 public class Exercicio06_Fibonacci {
     public static void main(String[] args) {
+        int numAtual = 1;
+        int numAnterior = 1;
+        int contador = 0;
+        int proximo;
 
-
+        while(contador<10) {
+            contador++;
+            System.out.println(numAnterior);
+            proximo = numAnterior + numAtual;
+            numAnterior = numAtual;
+            numAtual = proximo;
+        }
 
     }
 }
