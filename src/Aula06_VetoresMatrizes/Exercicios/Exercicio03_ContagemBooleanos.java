f2cad026b4c1ead4287f349844b81f0e281a34fe
Samuel Correia <thenenouno1@gmail.com>
2024-11-21 09:27:08 -0300

    Aula 21/11/2024

---
+++ src/Aula06_VetoresMatrizes/Exercicios/Exercicio03_ContagemBooleanos.java
@@ -0,0 +1,10 @@
+package Aula06_VetoresMatrizes.Exercicios;
+
+public class Exercicio03_ContagemBooleanos {
+    public static void main(String[] args) {
+        boolean[] vetorBoolean = {true,false,true,true,false,false,true,false};
+        for (boolean valorBoolean : vetorBoolean) {
+
+        }
+    }
+}
